52d4fa9c7933eba38b574848d664e014dbd50e73
hurley87 <dhurls99@gmail.com>
2016-03-06 16:18:53 -0500

    add progress bar and link to new itemem

--- client/modules/core/components/MainLayout.jsx
+++ client/modules/core/components/MainLayout.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Grid, Row, Col, PageHeader } from 'react-bootstrap';
+import ItemProgress from '../../items/components/ItemProgress.jsx';
 
 const Layout = ({content}) => (
   <Grid>
     <Row>
       <Col xs={12}>
         <PageHeader>Impossible List</PageHeader>
+      </Col>
+    </Row>
+    <Row>
+      <Col xs={12}>
+        <ItemProgress />
       </Col>
     </Row>
     {content()}
